03a8d8a012a292099ecfd057f5e4f53a8050e328
rayGreycloud <rayGreycloud@gmail.com>
2018-05-25 09:36:52 -0700

    Refactor with describe blocks

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -12,35 +12,32 @@ afterEach(() => {
   wrapped.unmount();
 });
 
-it('should have a text area and a button', () => {
-  expect(wrapped.find('textarea').length).toEqual(1);
-  expect(wrapped.find('button').length).toEqual(1);
+describe('Comment box', () => {
+  it('should have a text area and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+  });
 });
 
-it('should have text area that user can type in', () => {
-  // Find element; simulate change event
-  wrapped.find('textarea').simulate('change', {
-    // Provide mock event object
-    target: { value: 'new comment' }
+describe('Text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
   });
 
-  // Force component to update
-  wrapped.update();
-
-  // Assert the textarea value has changed
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
-
-it('should clear the text area when input is submitted', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+  it('should have allow user input', () => {
+    // Assert the textarea value has changed
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
   });
-  wrapped.update();
 
-  // Find form; simulate submit event
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
+  it('should clear when the user submits the form', () => {
+    // Find form; simulate submit event
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
 
-  // Assert the textarea has been cleared
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+    // Assert the textarea has been cleared
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
