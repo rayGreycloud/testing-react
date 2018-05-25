5588290206de09f7370522349f851c4d924103cb
rayGreycloud <rayGreycloud@gmail.com>
2018-05-24 19:53:40 -0700

    Convert to class based component; render form; make textarea a controlled input

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return (
-    <div>
-      <h2>Comment Box Component</h2>
-    </div>
-  );
-};
+class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = e => {
+    this.setState({ comment: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    // TODO - Call an action creator
+    // TODO - Save the comment
+
+    this.setState({ comment: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
