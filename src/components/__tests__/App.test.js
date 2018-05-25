9e78e7198dd026d6a354c3f996c515103a74a8f1
rayGreycloud <rayGreycloud@gmail.com>
2018-05-25 09:37:20 -0700

    Refactor using describe block

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,16 +4,18 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-let wrapped;
+describe('App', () => {
+  let wrapped;
 
-beforeEach(() => {
-  wrapped = shallow(<App />);
-});
+  beforeEach(() => {
+    wrapped = shallow(<App />);
+  });
 
-it('should show a comment box', () => {
-  expect(wrapped.find(CommentBox).length).toEqual(1);
-});
+  it('should show a comment box', () => {
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+  });
 
-it('should show a comment list', () => {
-  expect(wrapped.find(CommentList).length).toEqual(1);
+  it('should show a comment list', () => {
+    expect(wrapped.find(CommentList).length).toEqual(1);
+  });
 });
