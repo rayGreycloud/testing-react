19f3bf707fd3445a24a2476485e9ae90aa8a6e22
rayGreycloud <rayGreycloud@gmail.com>
2018-05-25 09:12:41 -0700

    Test: User can type in textarea; Textarea is cleared on form submit

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,31 @@ it('should have a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1);
   expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('should have text area that user can type in', () => {
+  // Find element; simulate change event
+  wrapped.find('textarea').simulate('change', {
+    // Provide mock event object
+    target: { value: 'new comment' }
+  });
+
+  // Force component to update
+  wrapped.update();
+
+  // Assert the textarea value has changed
+  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+});
+
+it('should clear the text area when input is submitted', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+
+  // Find form; simulate submit event
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+
+  // Assert the textarea has been cleared
+  expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
