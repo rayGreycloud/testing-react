ad42430bc500a9717ffea4d059f56bfe383962b5
rayGreycloud <rayGreycloud@gmail.com>
2018-05-24 19:54:56 -0700

    Test CommentBox component

---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import CommentBox from 'components/CommentBox';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it('should have a text area and a button', () => {
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+});
