976d968f63088c0df652bbad29d8368889ab8c93
rayGreycloud <rayGreycloud@gmail.com>
2018-05-24 19:51:11 -0700

    Use absolute path imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App';
-import CommentList from './components/CommentList';
+import App from 'components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
