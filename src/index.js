400b3b847f549eb473e33437a6c4f2ec9a471be3
rayGreycloud <rayGreycloud@gmail.com>
2018-05-26 10:27:42 -0700

    Add redux store and wrap App with Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
 
 import App from 'components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const store = createStore(
+  reducers,
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
