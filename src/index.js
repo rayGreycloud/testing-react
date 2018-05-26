25d6375075a6297a4cc925e63618ac7371ac33c9
rayGreycloud <rayGreycloud@gmail.com>
2018-05-26 12:22:01 -0700

    Import new Provider function and refactor

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
+import Root from 'Root';
 
 import App from 'components/App';
 
-const store = createStore(
-  reducers,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
 ReactDOM.render(
-  <Provider store={store}>
+  <Root>
     <App />
-  </Provider>,
+  </Root>,
   document.querySelector('#root')
 );
