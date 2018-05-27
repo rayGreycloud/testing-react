819f3aad458004c0bc90344e0e1cb0d2f3a634dc
rayGreycloud <rayGreycloud@gmail.com>
2018-05-27 07:33:21 -0700

    Pull out new comment text into testData variable

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,7 +17,7 @@ afterEach(() => {
   wrapped.unmount();
 });
 
-describe('Comment box', () => {
+describe('Comment Box', () => {
   it('should have a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
@@ -25,16 +25,18 @@ describe('Comment box', () => {
 });
 
 describe('Text area', () => {
+  const testData = 'New Comment';
+
   beforeEach(() => {
     wrapped.find('textarea').simulate('change', {
-      target: { value: 'new comment' }
+      target: { value: testData }
     });
     wrapped.update();
   });
 
   it('should have allow user input', () => {
     // Assert the textarea value has changed
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    expect(wrapped.find('textarea').prop('value')).toEqual(testData);
   });
 
   it('should clear when the user submits the form', () => {
