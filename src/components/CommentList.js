2110f52ba5ccfbd56348d26870f7917721d413dc
rayGreycloud <rayGreycloud@gmail.com>
2018-05-27 14:10:58 -0700

    Refactor as class component that renders comment list from state comments array

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default () => {
-  return (
-    <div>
-      <h2>Comment List Component</h2>
-    </div>
-  );
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => <li key={comment}>{comment}</li>);
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>{this.renderComments()}</ul>
+      </div>
+    );
+  }
+}
+
+CommentList.propTypes = {
+  comments: PropTypes.array.isRequired
 };
+
+const mapStateToProps = state => ({
+  comments: state.comments
+});
+
+export default connect(mapStateToProps)(CommentList);
