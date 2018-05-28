4530c6b3ddc04def5dccad711683a1910a6d3928
rayGreycloud <rayGreycloud@gmail.com>
2018-05-28 15:56:46 -0700

    Remove redux devtools

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
@@ -8,11 +8,7 @@ const Root = ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    compose(
-      applyMiddleware(reduxPromise),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(reduxPromise)
   );
 
   return <Provider store={store}>{children}</Provider>;
