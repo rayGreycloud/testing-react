3e7c855bd5cea9ff7c105583ac02b1f16181ade3
rayGreycloud <rayGreycloud@gmail.com>
2018-05-28 08:49:22 -0700

    Refactor store and add reduxPromise middleware

--- src/Root.js
+++ src/Root.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 const Root = ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+      applyMiddleware(reduxPromise),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 
   return <Provider store={store}>{children}</Provider>;
